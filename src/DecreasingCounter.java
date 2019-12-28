7f90e8b1286be223f5577030ddbb93b86e5082d5
Yana Smelyk <smelikyana@gmail.com>
2019-12-28 21:20:43 +0200

    additional commit

---
+++ src/DecreasingCounter.java
@@ -0,0 +1,19 @@
+public class DecreasingCounter {
+    private int value;
+
+    public DecreasingCounter(int valueAtStart) {
+        this.value = valueAtStart;
+    }
+
+    public void printValue() {
+        System.out.println("value: " + this.value);
+    }
+
+    public void decrease() {
+        if (this.value > 0) {
+            this.value--;
+        }
+
+    }
+}
+
