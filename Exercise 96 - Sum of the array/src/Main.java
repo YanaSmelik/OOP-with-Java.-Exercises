d77e77a716b6c12500dc6d451070688da1c7b087
YanaSmelik <39877972+YanaSmelik@users.noreply.github.com>
2020-01-06 13:37:36 +0200

    Exercise 96: Sum of the array
    The method public static sum(int[] array) returns the sum of the numbers in the array given as parameter.

---
+++ Exercise 96 - Sum of the array/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public static int sum(int[] array) {
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        return sum;
+    }
+
+}
